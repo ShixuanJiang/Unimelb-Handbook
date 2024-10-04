35e50f689824ab9e98f822afab7bbea5cc152550
Haomzhou1 <haomzhou1@student.unimelb.edu.au>
2024-10-05 00:24:15 +1000

    refactor: import Router

--- handbook-ui-react/src/App.jsx
+++ handbook-ui-react/src/App.jsx
@@ -1,17 +1,14 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import './index.css'
-import WelcomePage from './pages/WelcomePage'
+import WelcomePage from './pages/WelcomePage';
 
-
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<WelcomePage />} />)
+);
 
 const App = () => {
-    return (
-        <>
-        <WelcomePage/>
-        </>
-
-    )
+    return <RouterProvider router={router} />;
 }
 
 export default App
-
