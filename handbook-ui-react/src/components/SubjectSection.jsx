025783673fff1d5208507b7cd4f907217e74817d
Haomzhou1 <haomzhou1@student.unimelb.edu.au>
2024-10-31 23:35:24 +1100

    refactor: style of code changed a little

--- handbook-ui-react/src/components/SubjectSection.jsx
+++ handbook-ui-react/src/components/SubjectSection.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import SubjectCard from "../containers/subjectCard/SubjectCard";
 
-const SubjectSection = () => {
+const SubjectSection = ({ isChecklistExpanded }) => {
   return (
     <div className="flex justify-center py-4">
-      <div className="flex w-full flex-col space-y-6">
+      <div
+        className={`flex w-full flex-col space-y-6 ${isChecklistExpanded ? "lg:w-3/4" : "lg:w-full"}`}
+      >
         {/* Year 2024 */}
         <div>
           <h2 className="text-3xl font-bold">2024</h2>
           {/* Semester 1 */}
           <div className="mb-3">
-            <h3 className=" text-lg font-bold text-gray-600">
-              Semester 1
-            </h3>
+            <h3 className="text-lg font-bold text-gray-600">Semester 1</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <SubjectCard />
               <SubjectCard />
@@ -22,9 +22,7 @@ const SubjectSection = () => {
           </div>
           {/* Semester 2 */}
           <div>
-            <h3 className="text-lg font-bold text-gray-600">
-              Semester 2
-            </h3>
+            <h3 className="text-lg font-bold text-gray-600">Semester 2</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <SubjectCard />
               <SubjectCard />
@@ -38,9 +36,7 @@ const SubjectSection = () => {
           <h2 className="text-3xl font-bold">2025</h2>
           {/* Semester 1 */}
           <div className="mb-3">
-            <h3 className=" text-lg font-bold text-gray-600">
-              Semester 1
-            </h3>
+            <h3 className="text-lg font-bold text-gray-600">Semester 1</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <SubjectCard />
               <SubjectCard />
@@ -50,9 +46,7 @@ const SubjectSection = () => {
           </div>
           {/* Semester 2 */}
           <div>
-            <h3 className="text-lg font-bold text-gray-600">
-              Semester 2
-            </h3>
+            <h3 className="text-lg font-bold text-gray-600">Semester 2</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <SubjectCard />
               <SubjectCard />
@@ -66,9 +60,7 @@ const SubjectSection = () => {
           <h2 className="text-3xl font-bold">2026</h2>
           {/* Semester 1 */}
           <div className="mb-3">
-            <h3 className=" text-lg font-bold text-gray-600">
-              Semester 1
-            </h3>
+            <h3 className="text-lg font-bold text-gray-600">Semester 1</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <SubjectCard />
               <SubjectCard />
@@ -78,9 +70,7 @@ const SubjectSection = () => {
           </div>
           {/* Semester 2 */}
           <div>
-            <h3 className="text-lg font-bold text-gray-600">
-              Semester 2
-            </h3>
+            <h3 className="text-lg font-bold text-gray-600">Semester 2</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <SubjectCard />
               <SubjectCard />
