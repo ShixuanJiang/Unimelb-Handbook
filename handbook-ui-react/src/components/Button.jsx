d17784ef9a2638d3e09b3fa8510ece4160c884a5
Haomzhou1 <haomzhou1@student.unimelb.edu.au>
2024-11-02 19:51:47 +1100

    feat: finish button UI

--- handbook-ui-react/src/components/Button.jsx
+++ handbook-ui-react/src/components/Button.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Button = () => {
+const Button = ({ onCheckEligibility }) => {
   return (
     <>
-      <div className="z-10 mr-[317px] flex justify-center">
+      <div className="fixed bottom-5 z-50 flex w-full max-w-sm justify-end p-2">
         <button
-          className="rounded px-3 mr-2 text-white"
+          onClick={onCheckEligibility}
+          className="mr-2 rounded px-3 py-2 text-white transition-colors duration-200 hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
           style={{ backgroundColor: "#35659E" }}
         >
           Check Eligibility
         </button>
         <button
-          className="rounded bg-green-400 px-3 text-black"
+          className="rounded bg-green-400 px-3 py-2 text-black transition-colors duration-200 hover:bg-green-700 focus:outline-none focus:ring focus:ring-green-300"
           style={{ backgroundColor: "#44DE5B" }}
         >
           Enroll
